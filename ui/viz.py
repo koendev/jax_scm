cf34166d8cd9247da95d7a4f6f22ab33e7b16dfb
Maximilian Pierzyna <m.pierzyna@tudelft.nl>
2026-02-13 15:29:38 +0100

    make viz more modular

--- ui/viz.py
+++ ui/viz.py
@@ -61,46 +61,44 @@ class TsPlot:
 class FieldPlot:
     def __init__(self, var: str):
         p = bp.figure(height=500, width=700)
-        color_mapper = bm.LinearColorMapper(
-            palette="Sunset11",
-            low=ds[var].values.min(),
-            high=ds[var].values.max(),
-        )
+        color_mapper = bm.LinearColorMapper(palette="Sunset11", low=0, high=1)  # dummy values
         img = p.image(
-            image=[ds[var].values.T],
+            image=[],  # will be populated by `_update_data` call
             x=t_start,
             y=0,
             dw=t_end - t_start,
             dh=H,
             color_mapper=color_mapper,
             level="image",
-            # source=store_2d,
         )
         p.x_range.range_padding = p.y_range.range_padding = 0
         p.x_range.bounds = (t_start, t_end)
         p.y_range.bounds = (0, H)
 
+        # Add color bar
         cbar = bm.ColorBar(color_mapper=color_mapper)
         p.add_layout(cbar, "right")
 
-        # Color bar ranges
+        # Color bar ranges selector
         def _vrange_callback(attr, old, new):
+            """Set colorbar to range from range selector"""
             color_mapper.low = new[0]
             color_mapper.high = new[1]
 
-        vmin = ds[var].values.min()
-        vmax = ds[var].values.max()
-        vrange = bm.RangeSlider(
-            start=ds[var].values.min(),
-            end=ds[var].values.max(),
-            value=(vmin, vmax),
-            step=(vmax - vmin) / 250,
-        )
+        vrange = bm.RangeSlider(start=0, end=1, value=(0, 1), step=10)  # dummy values
         vrange.on_change("value", _vrange_callback)
 
-        def _update_data(data: np.ndarray):
-            # Clip data if log introduced inf
-            data = np.where(np.isfinite(data), data, np.nan)
+        def _update_data(var: str | None, use_log: bool):
+            """Load image data, apply log, and update image, color mapper, and color range selector."""
+            if var is None:
+                var = self.var  # reuse currently selected variable
+            else:
+                self.var = var  # update currently selected variable
+
+            # Load data
+            data = ds[var].values.T
+            data = np.log10(data) if use_log else data
+            data = np.where(np.isfinite(data), data, np.nan)  # log can introduce -inf, so force to NaN
 
             # Update image
             img.data_source.data["image"] = [data]
@@ -119,28 +117,22 @@ class FieldPlot:
 
         # Variable selector
         def _sel_callback(attr, old, new_var):
-            # Select new data and transform if log scale is on
-            use_log = self.log_toggle.active
-            data = ds[new_var].values.T
-            data = np.log10(data) if use_log else data
-
-            # Then update
-            _update_data(data)
+            _update_data(var=new_var, use_log=self.log_toggle.active)
 
         select = bm.Select(title="Select variable", options=vars_2d, value=var)
         select.on_change("value", _sel_callback)
 
         # Log scale toggle
         def _log_callback(attr, old, use_log):
-            # Get currently selected variable's data and transform
-            data = img.data_source.data["image"][0]
-            data = np.log10(data) if use_log else 10**data
-
-            # Then update
-            _update_data(data)
+            """Keep current variable, but update log scale"""
+            _update_data(var=None, use_log=use_log)
 
         log_toggle = bm.Switch(label="Log scale")
         log_toggle.on_change("active", _log_callback)
+
+        # Now set data
+        self.var = var
+        _update_data(var=var, use_log=False)
 
         self.p = p
         self.select = select
