9eaf5a3435f4225166426d65f655fd06dac78b84
Maximilian Pierzyna <m.pierzyna@tudelft.nl>
2026-05-04 11:27:36 +0200

    optimize consistency. safe root should behave the same in grad and non-grad mode

--- src/scm/safe_math.py
+++ src/scm/safe_math.py
@@ -14,23 +14,24 @@ GRAD_MIN, GRAD_MAX = -1e6, 1e6
 
 
 @jax.custom_vjp
-def safe_pow(x: jnp.ndarray, p: float, eps: float = 1e-6) -> jnp.ndarray:
-    """Safe power function with argument and gradient clipping to prevent NaNs."""
-    # Default forward: gets called outside gradient computation, so no clipping to ensure correct results.
-    return jnp.pow(x, p)
-
-
-def safe_pow_fwd(x: jnp.ndarray, p: float, eps: float):
-    # Forward in gradient computation: clip x to eps to prevent NaNs.
+def safe_root(x: jnp.ndarray, p: float, eps: float = 1e-6) -> jnp.ndarray:
+    """Safe root function with argument and gradient clipping to prevent NaNs in backprop."""
+    # Default forward: gets called outside gradient computation
     x_safe = jnp.maximum(x, eps)
-    return safe_pow(x_safe, p, eps), (x_safe, p)  # pass x_safe and p as residuals for backward
+    return jnp.pow(x_safe, p)
 
 
-def safe_pow_bwd(res, g):
-    # Backward: compute gradient with safe_x and clip the gradient
+def safe_root_fwd(x: jnp.ndarray, p: float, eps: float):
+    # Forward in gradient computation. Same as default forward.
+    x_safe = jnp.maximum(x, eps)
+    return safe_root(x_safe, p, eps), (x_safe, p)  # pass x_safe and p as residuals for backward
+
+
+def safe_root_bwd(res, g):
+    # Backward in gradient computation: compute safe gradient
     x_safe, p = res
     raw = g * p * jnp.power(x_safe, p - 1)
     return (jnp.clip(raw, GRAD_MIN, GRAD_MAX), None, None)  # p and eps are non diffable, so return None.
 
 
-safe_pow.defvjp(safe_pow_fwd, safe_pow_bwd)
+safe_root.defvjp(safe_root_fwd, safe_root_bwd)
